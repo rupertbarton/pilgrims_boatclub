8e77e2466034413012641e50f8bb407040b3e8ce
Rupert Barton <bartonrupert@hotmail.com>
2020-09-03 10:52:43 +0100

    Add redirect from junk url to home

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from '../components/navigation/navBar';
 import PaperDisplay from '../components/content/paperDisplay';
 import routes from '../routes';
@@ -13,6 +13,7 @@ const App = () => (
           <PaperDisplay>{route.component}</PaperDisplay>
         </Route>
       ))}
+      <Redirect from="*" to="/" />
     </Switch>
   </Router>
 );
